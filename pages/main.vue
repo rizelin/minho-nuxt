5462db83969bd2923bf440bc86d7542ab5a6d172
BMH <minhobaek@Mino-MacBook-Pro.local>
2023-01-10 18:29:14 +0900

    쇼핑 상품 목록 페이지와 상세 페이지 개발-상품 목록 표시 기능 구현

--- pages/main.vue
+++ pages/main.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
     <p>메인 페이지입니다</p>
-    {{ products }}
+    <div>
+      <ul>
+        <li v-for="product in products" :key="product.id">
+          <img :src="product.imageUrl" :alt="product.name" />
+          <p>{{ product.name }}</p>
+          <p>{{ product.price }}</p>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -11,7 +19,12 @@ import axios from 'axios';
 export default {
   async asyncData(){
     const response = await axios.get('http://localhost:3000/products')
-    const products = response.data
+    const products = response.data.map((item) => {
+      return{
+        ...item,
+        imageUrl:`${item.imageUrl}?random=${Math.random()}`
+      }
+    })
     return {
       products
     }
