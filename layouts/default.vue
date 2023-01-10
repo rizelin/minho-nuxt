0fd738e60673066122c93c42a6cf6ad4e0be9661
BMH <minhobaek@Mino-MacBook-Pro.local>
2023-01-10 11:47:15 +0900

    Nuxt프로젝트시작-nuxt-link태그 소개

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <h1>{{ $route.name }} 페이지</h1>
+    <NuxtLink to="/">홈페이지</NuxtLink>
+    <NuxtLink to="/main">메인 페이지</NuxtLink>
+    <NuxtLink to="/product">상품 페이지</NuxtLink>
     <Nuxt />
   </div>
 </template>
