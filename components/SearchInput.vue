29416142587666b722d5e0cf76fccf14f35a3496
BMH <minhobaek@Mino-MacBook-Pro.local>
2023-01-11 21:06:16 +0900

    쇼핑상품검색UI개발-해더와 검색 UI스타일정리

--- components/SearchInput.vue
+++ components/SearchInput.vue
@@ -1,11 +1,12 @@
 <template>
-  <div>
+  <div class="input-wrapper flex">
     <input
       type="text"
+      class="search-input"
       :value="value"
       @input="$emit('input', $event.target.value)"
     />
-    <button type="button" @click="$emit('search')">Search</button>
+    <button class="btn" type="button" @click="$emit('search')">Search</button>
   </div>
 </template>
 
@@ -21,5 +22,17 @@ export default {
 </script>
 
 <style>
-
+.input-wrapper {
+  height: 40px;
+  margin: 1rem 0;
+}
+.search-input {
+  width: 200px;
+  font-size: 1.2rem;
+  font-weight: 500;
+}
+.btn {
+  font-size: 1.2rem;
+  font-weight: 500;
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -62,7 +62,11 @@ export default {
     },
     async searchProducts() {
       const response = await fetchProductsByKeyword(this.searchKeyword);
-      console.log(response)
+      console.log(response.data)
+      this.products = response.data.map((item) => ({
+        ...item,
+        imageUrl:`${item.imageUrl}?random=${Math.random()}`,
+      }))
     },
     // updateSearchKeyword(keyword) {
     //  this.searchKeyword = keyword
@@ -76,6 +80,37 @@ export default {
 }
 </script>
 
-<style>
 
+<style scoped>
+.flex {
+  display: flex;
+  justify-content: center;
+}
+.item {
+  display: inline-block;
+  width: 400px;
+  height: 300px;
+  text-align: center;
+  margin: 0 0.5rem;
+  cursor: pointer;
+}
+.product-image {
+  width: 400px;
+  height: 250px;
+}
+.app {
+  position: relative;
+}
+.cart-wrapper {
+  position: sticky;
+  float: right;
+  bottom: 50px;
+  right: 50px;
+}
+.cart-wrapper .btn {
+  display: inline-block;
+  height: 40px;
+  font-size: 1rem;
+  font-weight: 500;
+}
 </style>
