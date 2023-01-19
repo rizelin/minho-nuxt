5152ff0aa15a4cc3f460baea68de5cd8b87a2729
BMH <minhobaek@Mino-MacBook-Pro.local>
2023-01-19 14:53:43 +0900

    장바구니 페이지 리팩토링 및 문제점 해결-장바구니 목록 조회 기능 API 연동

--- store/index.js
+++ store/index.js
@@ -1,3 +1,8 @@
+import { fetchCartItems } from "~/api"
+
+export const FETCH_CART_ITEMS = 'FETCH_CART_ITEMS'
+
+// store
 export const state = () => ({
   cartItems: [],
 })
@@ -9,5 +14,16 @@ export const mutations = {
       imageUrl: `${cartItem.imageUrl}?random=${Math.random()}`,
     }
     state.cartItems.push(newCartItem)
+  },
+  setCartItems(state, cartItems) {
+    state.cartItems = cartItems
+  },
+}
+
+export const actions = {
+  async [FETCH_CART_ITEMS]({ commit }) {
+    const { data } = await fetchCartItems()
+    commit('setCartItems', data)
+    // return response
   }
 }
